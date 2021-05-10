720b8332678eca8ce7d08efe5ec4687ccacd231a
Domenico159 <domenico.zummo@gmail.com>
2021-05-10 21:19:41 +0200

    Add if adult or child

--- src/components/Col.vue
+++ src/components/Col.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="col">
       <!-- Movie -->
-      <ul v-if="genre == 'movie'">
+      <ul>
           <li
           v-for="(ele,index) in filmsArray"
           :key="index">
               <img :src="`${urlImg}${ele.poster_path}`" alt="">
               <div class="overlay">
-                  <h2>{{ele.title}}</h2>
-                  <h2>{{ele.original_title}}</h2>
+                  <h2>{{genre == 'movie' ? ele.title : ele.name }}</h2>
+                  <h2>{{genre == 'movie' ? ele.original_title : ele.original_name }}</h2>
                   <img class="lan-en" v-if="ele.original_language == 'en'" src="@/assets/img/flags-boolflix/en.png" alt="">
                   <img  class="lan-it" v-else-if="ele.original_language == 'it'" src="@/assets/img/flags-boolflix/it.png" alt="">
                   <h2 v-else>{{ele.original_language}}</h2>
@@ -22,30 +22,10 @@
                   <div class="overview">
                   </div>
                  </div>
-                  
-                  
-              </div>
-          </li>
-      </ul>
-      <!-- Serie tv -->
-        <ul v-else>
-          <li v-for="(ele,index) in filmsArray"
-          :key="index">
-              <img :src="`${urlImg}${ele.poster_path}`" alt="">
-              <div class="overlay">
-                  <h2>{{ele.name}}</h2>
-                  <h2>{{ele.original_name}}</h2>
-                  <img class="lan-en" v-if="ele.original_language == 'en'" src="@/assets/img/flags-boolflix/en.png" alt="">
-                  <img  class="lan-it" v-else-if="ele.original_language == 'it'" src="@/assets/img/flags-boolflix/it.png" alt="">
-                  <h2 v-else>{{ele.original_language}}</h2>
-                  <div class="stars">
-                      <i v-for="(e,index) in 5"
-                  :key="index"
-                  class="fas fa-star"
-                   :class="{ 'active' : index <  Math.floor(ele.vote_average / 2)}"
-                  >
-                  </i> 
-                  </div>
+                 <div class="adult-or-child">
+                     <i :class="{adult:ele.adult}"
+                     class="fas fa-user-circle"></i>
+                 </div>
               </div>
           </li>
       </ul>
@@ -106,12 +86,19 @@ ul{
             display: flex;
             flex-direction: column;
             justify-content: space-around;
+            align-items: center;
             .fas {
                 margin-right: 2px;
                 font-size: 24px;
             }
             .fas.active{
                 color: yellow;
+            }
+            .fa-user-circle{
+                color: rgb(81, 161, 27);
+            }
+            .fa-user-circle.adult{
+                color: rgb(161, 27, 27);
             }
 
             h2{
